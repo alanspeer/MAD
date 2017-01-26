2a776e88fe2c2d1153359fa0046b0cab61216b15
Alan <2speea07@solent.ac.uk>
2017-01-26 10:49:28 +0000

    added calculate age code

--- app/src/main/java/com/example/a2speea07/helloworld/MainActivity.java
+++ app/src/main/java/com/example/a2speea07/helloworld/MainActivity.java
@@ -2,17 +2,31 @@ package com.example.a2speea07.helloworld;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends Activity implements OnClickListener{
+    /** Called when the activity is first created. */
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button b = (Button)findViewById(R.id.btn1);
+        b.setOnClickListener(this);
 
-        TextView tv = new TextView(this);
-        tv.setText("HelloWorld!!!!");
-        setContentView(tv);
+
     }
+
+    public void onClick(View view)
+    {
+        TextView tv = (TextView)findViewById(R.id.tv1);
+        EditText et = (EditText)findViewById(R.id.et1);
+        int yearOfBirth = Integer.parseInt(et.getText().toString());
+        int age = 2016 - yearOfBirth;
+        tv.setText("Your age is: " + String.valueOf(age));
+    }
+
 }

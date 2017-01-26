f92b13258ac4215e947da52a3e0573c8d18d282f
Alan <2speea07@solent.ac.uk>
2017-01-26 10:08:34 +0000

    Added a text view!

--- app/src/main/java/com/example/a2speea07/helloworld/MainActivity.java
+++ app/src/main/java/com/example/a2speea07/helloworld/MainActivity.java
@@ -10,9 +10,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        //TextView tv = new TextView(this);
-       // tv.setText("HelloWorld!!!!");
-        //setContentView(tv);
+
+        TextView tv = new TextView(this);
+        tv.setText("HelloWorld!!!!");
+        setContentView(tv);
     }
 }

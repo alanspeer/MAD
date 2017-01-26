82c2d1308574626e0c4ff141abbaf82415771f80
Alan Speer <Alan Speer>
2017-01-26 09:58:17 +0000

    Added my hello world application.

---
+++ app/src/main/java/com/example/a2speea07/helloworld/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.a2speea07.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        //TextView tv = new TextView(this);
+       // tv.setText("HelloWorld!!!!");
+        //setContentView(tv);
+    }
+}
